109ce8196fa46601abf800e0289eabf24a793734
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-26 12:31:33 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,59 +1,57 @@
--- main.lua: Główny plik gry, ładuje moduły i obsługuje główne pętle
-
-require("conf")
 require("player")
 require("levels")
 require("menu")
 require("settings")
 require("utils")
-require("audio")
-
 function love.load()
-love.window.setTitle("Hacker Game")
-initAudio() -- Z audio.lua
-applySettings() -- Z settings.lua
-
-state = 'menu'
-gameOver = false
-gameWon = false
-currentLevel = 1
-score = 0
-lives = 3 -- Nowy system żyć
-highScore = loadHighScore() -- Z utils.lua
-highestLevel = loadHighestLevel() -- Z utils.lua
-gameMode = 'normal'
-timeElapsed = 0
-timeLimit = 0
-achievements = loadAchievements() -- Z utils.lua
-
-loadLevel(currentLevel) -- Z levels.lua
-end
-
-function love.update(dt)
-if state == 'game' then
-    updateGame(dt) -- Z levels.lua
-    updateAudioVolumes(dt) -- Z audio.lua, poprawiono z updateAudio
-    end
-    end
-
-    function love.draw()
-    if state == 'game' then
-        drawGame() -- Z levels.lua
-        elseif state == 'menu' then
-            drawMenu() -- Z menu.lua
-            elseif state == 'level_select' then
-                drawLevelSelect() -- Z menu.lua
-                elseif state == 'game_modes' then
-                    drawGameModes() -- Z menu.lua
-                    elseif state == 'settings' then
-                        drawSettings() -- Z settings.lua
-                        elseif state == 'pause' then
-                            drawPause() -- Z menu.lua
-                            elseif state == 'achievements' then
-                                drawAchievements() -- Z menu.lua
-                                end
-                                end
-
-                                function love.keypressed(key)
-                                handleKeyPressed(key) -- Z menu.lua, settings.lua, player.lua
-                                end
+love.window.setTitle("Bit Jump")
+applySettings()
+logo = nil -- Initialize logo as nil
+if love.filesystem.getInfo("Bit-Jump.png") then -- Check if file exists
+    logo = love.graphics.newImage("Bit-Jump.png")
+    else
+        print("Warning: Bit-Jump.png not found. Using default placeholder text.")
+        end
+        state = 'menu'
+        gameOver = false
+        gameWon = false
+        currentLevel = 1
+        score = 0
+        lives = 3
+        highScore = loadHighScore()
+        highestLevel = loadHighestLevel()
+        gameMode = 'normal'
+        timeElapsed = 0
+        timeLimit = 0
+        achievements = loadAchievements()
+        loadLevel(currentLevel)
+        end
+        function love.update(dt)
+        if state == 'game' then
+            updateGame(dt)
+            end
+            end
+            function love.draw()
+            if state == 'game' then
+                drawGame()
+                elseif state == 'menu' then
+                    drawMenu()
+                    elseif state == 'level_select' then
+                        drawLevelSelect()
+                        elseif state == 'game_modes' then
+                            drawGameModes()
+                            elseif state == 'settings' then
+                                drawSettings()
+                                elseif state == 'pause' then
+                                    drawPause()
+                                    elseif state == 'achievements' then
+                                        drawAchievements()
+                                        elseif state == 'credits' then
+                                            drawCredits() -- Nowa sekcja
+                                            elseif state == 'themes' then
+                                                drawThemes() -- Nowa sekcja dla themes
+                                                end
+                                                end
+                                                function love.keypressed(key)
+                                                handleKeyPressed(key)
+                                                end
