0ddb4909834a4ff90dbb92e7feae54f8aaf68372
VoidArc Studio <chudybyczek2137@gmail.com>
2025-09-26 12:32:03 +0200

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,11 +1,9 @@
--- conf.lua: Konfiguracja Love2D
-
 function love.conf(t)
 t.window.width = 800
 t.window.height = 600
 t.window.resizable = true
 t.window.fullscreen = false
-t.window.title = "Hacker Game"
-t.modules.audio = true
-t.modules.sound = true
+t.window.title = "Bit Jump"
+t.modules.audio = false
+t.modules.sound = false
 end
