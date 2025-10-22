bfb7da9f9cec3979cdc941fa89f22d1c973e2ead
michal92299 <chudybyczek2137@gmail.com>
2025-10-22 20:26:45 +0200

    Create renderer.js

---
+++ app/renderer.js
@@ -0,0 +1,18 @@
+const { ipcRenderer } = require('electron');
+
+function launchGame(game) {
+  const gamePaths = {
+    'starblaster': '/usr/share/HackerOS/Scripts/HackerOS-Games/starblaster',
+    'bit-jump': '/usr/share/HackerOS/Scripts/HackerOS-Games/bit-jump.love',
+    'the-racer': '/usr/share/HackerOS/Scripts/HackerOS-Games/the-racer'
+  };
+
+  const launchCommands = {
+    'starblaster': gamePaths['starblaster'],
+    'bit-jump': `love ${gamePaths['bit-jump']}`,
+    'the-racer': gamePaths['the-racer']
+  };
+
+  // Send the command to the main process to execute
+  ipcRenderer.send('launch-game', launchCommands[game]);
+}
