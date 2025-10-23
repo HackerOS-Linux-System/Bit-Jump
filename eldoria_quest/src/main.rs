aa992bb9a7fe5427c9d94275914e3fceb73e902b
michal92299 <chudybyczek2137@gmail.com>
2025-10-23 15:02:17 +0200

    Update main.rs

--- eldoria_quest/src/main.rs
+++ eldoria_quest/src/main.rs
@@ -1,87 +1,55 @@
 use bevy::prelude::*;
-use bevy::window::WindowMode;
-mod components;
-mod systems;
-mod resources;
-mod map;
-mod menu;
-mod quests;
-mod combat;
-mod inventory;
-mod crafting;
-mod factions;
-mod environment;
-mod skills;
-mod trading;
-mod events;
-mod save_load;
+use bevy::window::{Window, WindowPlugin};
 
-use components::*;
-use systems::*;
-use resources::*;
-use map::*;
-use menu::*;
-use quests::*;
-use combat::*;
-use inventory::*;
-use crafting::*;
-use factions::*;
-use environment::*;
-use skills::*;
-use trading::*;
-use events::*;
-use save_load::*;
+mod map;
+mod player;
+mod npc;
+mod game;
+
+use game::{GameState, GameResources, setup_camera, update_player_movement, render_map, render_entities, handle_interactions, setup_main_menu, handle_menu_buttons, setup_character_creation, handle_class_selection, setup_pause_menu, handle_pause_buttons, setup_settings, handle_settings};
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             title: "Eldoria Quest".into(),
-                             resolution: (1280.0, 720.0).into(),
+                             resolution: (800.0, 600.0).into(),
                              ..default()
         }),
         ..default()
     }))
+    .init_resource::<GameResources>()
     .init_state::<GameState>()
-    .init_resource::<GameSettings>()
-    .init_resource::<Quests>()
-    .init_resource::<Inventory>()
-    .init_resource::<Factions>()
-    .init_resource::<Environment>()
-    .init_resource::<Skills>()
-    .init_resource::<RandomEvents>()
-    .add_plugins(MenuPlugin)
-    .add_plugins(GamePlugin)
-    .add_plugins(MapPlugin)
-    .add_plugins(QuestPlugin)
-    .add_plugins(CombatPlugin)
-    .add_plugins(InventoryPlugin)
-    .add_plugins(CraftingPlugin)
-    .add_plugins(FactionPlugin)
-    .add_plugins(EnvironmentPlugin)
-    .add_plugins(SkillsPlugin)
-    .add_plugins(TradingPlugin)
-    .add_plugins(EventsPlugin)
     .add_systems(Startup, setup_camera)
-    .add_systems(Update, toggle_fullscreen)
+    .add_systems(Update, (
+        update_player_movement.run_if(in_state(GameState::InGame)),
+                          handle_interactions.run_if(in_state(GameState::InGame)),
+                          render_map.run_if(in_state(GameState::InGame)),
+                          render_entities.run_if(in_state(GameState::InGame)),
+                          handle_menu_buttons.run_if(in_state(GameState::MainMenu)),
+                          handle_class_selection.run_if(in_state(GameState::CharacterCreation)),
+                          handle_pause_buttons.run_if(in_state(GameState::Pause)),
+                          handle_settings.run_if(in_state(GameState::Settings)),
+    ))
+    .add_systems(OnEnter(GameState::MainMenu), setup_main_menu)
+    .add_systems(OnEnter(GameState::CharacterCreation), setup_character_creation)
+    .add_systems(OnEnter(GameState::Pause), setup_pause_menu)
+    .add_systems(OnEnter(GameState::Settings), setup_settings)
+    .add_systems(Update, handle_state_transitions)
     .run();
 }
 
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 1000.0),
-                   ..default()
-    });
-}
-
-fn toggle_fullscreen(keyboard: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window>) {
-    if keyboard.just_pressed(KeyCode::KeyF) {
-        if let Ok(mut window) = windows.get_single_mut() {
-            window.mode = if window.mode == WindowMode::Fullscreen {
-                WindowMode::Windowed
-            } else {
-                WindowMode::Fullscreen
-            };
+fn handle_state_transitions(
+    mut next_state: ResMut<NextState<GameState>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    current_state: Res<State<GameState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        match current_state.get() {
+            GameState::InGame => next_state.set(GameState::Pause),
+            GameState::Pause => next_state.set(GameState::InGame),
+            GameState::Settings => next_state.set(GameState::MainMenu),
+            _ => {},
         }
     }
 }
